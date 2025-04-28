8bb56fc0906e342b4abe5ef272ba8716f99b3ce5
sharkLoc <mmtinfo@163.com>
2025-04-28 11:37:53 +0800

    updata code in src/cli/rename.rs

--- src/cli/rename.rs
+++ src/cli/rename.rs
@@ -23,59 +23,38 @@ pub fn rename_fastq(
     let mut fo = fastq::Writer::new(file_writer(output, compression_level, stdout_type)?);
     let mut n: usize = 0;
 
-    if let Some(pre) = prefix {
-        for rec in fp.records().map_while(Result::ok) {
-            n += 1;
-            /*let newid = match label {
-                Some(x) => {
-                    if before {
-                        format!("{}{}{}",x,pre,n)
-                    } else {
-                        format!("{}{}{}",pre,x,n)
-                    }
-                },
-                None => { format!("{}{}",pre,n) }
-            };*/
-            let newid = if before {
-                format!("{}{}{}", label.unwrap_or(&String::new()), pre, n)
-            } else {
-                format!("{}{}{}", pre, label.unwrap_or(&String::new()), n)
-            };
-
-            let record = if keep {
-                Record::with_attrs(&newid, rec.desc(), rec.seq(), rec.qual())
-            } else {
-                Record::with_attrs(&newid, None, rec.seq(), rec.qual())
-            };
-            fo.write_record(&record)?;
-        }
-        fo.flush()?;
-    } else {
-        for rec in fp.records().map_while(Result::ok) {
-            n += 1;
-            /*let newid = if let Some(x) = label {
+    let new_rec = |rec: Record, n: usize| -> Record {
+        let newid = match &prefix {
+            Some(pre) => {
                 if before {
-                    format!("{}{}",x,rec.id())
+                    format!("{}{}{}", label.unwrap_or(&String::new()), pre, n)
                 } else {
-                    format!("{}{}",rec.id(),x)
+                    format!("{}{}{}", pre, label.unwrap_or(&String::new()), n)
                 }
-            } else {
-                format!("{}",rec.id())
-            };*/
-            let newid = if before {
-                format!("{}{}", label.unwrap_or(&String::new()), rec.id())
-            } else {
-                format!("{}{}", rec.id(), label.unwrap_or(&String::new()))
-            };
-            let record = if keep {
-                Record::with_attrs(&newid, rec.desc(), rec.seq(), rec.qual())
-            } else {
-                Record::with_attrs(&newid, None, rec.seq(), rec.qual())
-            };
-            fo.write_record(&record)?;
+            }
+            None => {
+                if before {
+                    format!("{}{}", label.unwrap_or(&String::new()), rec.id())
+                } else {
+                    format!("{}{}", rec.id(), label.unwrap_or(&String::new()))
+                }
+            }
+        };
+
+        if keep {
+            Record::with_attrs(&newid, rec.desc(), rec.seq(), rec.qual())
+        } else {
+            Record::with_attrs(&newid, None, rec.seq(), rec.qual())
         }
-        fo.flush()?;
+    };
+
+    for rec in fp.records().map_while(Result::ok) {
+        n += 1;
+        let record = new_rec(rec, n);
+        fo.write_record(&record)?;
     }
+    fo.flush()?;
+
 
     info!("total rename sequence number: {}", n);
     Ok(())
