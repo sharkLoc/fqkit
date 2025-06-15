a5cef3090b019ce683b9edec9009c86fb892eaa2
sharkLoc <mmtinfo@163.com>
2025-06-15 15:39:09 +0800

    use paraseq in subcmd reverse

--- src/cli/reverse.rs
+++ src/cli/reverse.rs
@@ -1,5 +1,6 @@
 use crate::{errors::FqkitError, utils::file_reader, utils::file_writer};
-use bio::io::fastq;
+use super::misc::write_record;
+use paraseq::fastq;
 use std::collections::HashMap;
 
 pub fn reverse_comp_seq(
@@ -9,8 +10,8 @@ pub fn reverse_comp_seq(
     compression_level: u32,
     stdout_type: char,
 ) -> Result<(), FqkitError> {
-    let fq_reader = file_reader(input).map(fastq::Reader::new)?;
-
+    let mut fq_reader = file_reader(input).map(fastq::Reader::new)?;
+    let mut rset = fastq::RecordSet::default();
     let maps = HashMap::from([
         (b'A', b'T'),
         (b'T', b'A'),
@@ -19,32 +20,24 @@ pub fn reverse_comp_seq(
         (b'N', b'N'),
     ]);
     let mut out_writer =
-        file_writer(out, compression_level, stdout_type).map(fastq::Writer::new)?;
+        file_writer(out, compression_level, stdout_type)?;
 
-    for rec in fq_reader.records().map_while(Result::ok) {
-        let rev_seq = rec.seq().iter().copied().rev().collect::<Vec<u8>>();
-        let rev_qual = rec.qual().iter().copied().rev().collect::<Vec<u8>>();
+    while rset.fill(&mut fq_reader)? {
+        for rec in rset.iter().map_while(Result::ok) {
+            let rev_seq = rec.seq().iter().copied().rev().collect::<Vec<u8>>();
+            let rev_qual = rec.qual().iter().copied().rev().collect::<Vec<u8>>();
 
-        let rc_seq = rev_seq
-            .iter()
-            .map(|x| maps.get(x).unwrap_or(&b'N'))
-            .collect::<Vec<&u8>>();
-        let rev_comp = rc_seq.iter().map(|x| **x).collect::<Vec<u8>>();
+            let rc_seq = rev_seq
+                .iter()
+                .map(|x| maps.get(x).unwrap_or(&b'N'))
+                .collect::<Vec<&u8>>();
+            let rev_comp = rc_seq.iter().map(|x| **x).collect::<Vec<u8>>();
 
-        if rev {
-            out_writer.write(
-                rec.id(),
-                rec.desc(),
-                rev_seq.as_slice(),
-                rev_qual.as_slice(),
-            )?;
-        } else {
-            out_writer.write(
-                rec.id(),
-                rec.desc(),
-                rev_comp.as_slice(),
-                rev_qual.as_slice(),
-            )?;
+            if rev {
+                write_record(&mut out_writer, rec.id(), rev_seq.as_slice(), rev_qual.as_slice())?;
+            } else {
+                write_record(&mut out_writer, rec.id(), rev_comp.as_slice(), rev_qual.as_slice())?;
+            }
         }
     }
     out_writer.flush()?;
