6dcc74e1eea16d120b9200923fce9d042b784945
sharkLoc <mmtinfo@163.com>
2025-06-15 15:10:00 +0800

    use paraseq in subcmd select

--- src/cli/select.rs
+++ src/cli/select.rs
@@ -1,7 +1,8 @@
 use crate::{errors::FqkitError, utils::file_reader, utils::file_writer};
-use bio::io::fastq;
+use super::misc::write_record;
+use paraseq::fastq;
 use log::info;
-use std::collections::HashMap;
+use std::collections::HashSet;
 
 pub fn select_pe_fastq(
     fq1: &String,
@@ -11,43 +12,58 @@ pub fn select_pe_fastq(
     compression_level: u32,
     stdout_type: char,
 ) -> Result<(), FqkitError> {
-    let mut read1_id = HashMap::new();
-    let mut read2_id = HashMap::new();
-    let fq_reader1 = file_reader(Some(fq1)).map(fastq::Reader::new)?;
-    let fq_reader2 = file_reader(Some(fq2)).map(fastq::Reader::new)?;
+    
+    let mut fq_reader1 = file_reader(Some(fq1)).map(fastq::Reader::new)?;
+    let mut fq_reader2 = file_reader(Some(fq2)).map(fastq::Reader::new)?;
+    let mut rset1 = fastq::RecordSet::default();
+    let mut rset2 = fastq::RecordSet::default();
 
-    for rec in fq_reader1.records().map_while(Result::ok) {
-        let k = rec.id().to_owned();
-        read1_id.entry(k).or_insert(());
+
+    let mut read1_id = HashSet::new();
+    let mut read2_id = HashSet::new();
+    while rset1.fill(&mut fq_reader1)? {
+        for rec in rset1.iter().map_while(Result::ok) {
+            read1_id.insert(rec.id().to_owned());
+        }
     }
-    for rec in fq_reader2.records().map_while(Result::ok) {
-        let k = rec.id().to_owned();
-        read2_id.entry(k).or_insert(());
+
+    while rset2.fill(&mut fq_reader2)? {
+        for rec in rset2.iter().map_while(Result::ok) {
+            read2_id.insert(rec.id().to_owned());
+        }
     }
-    info!("output selected read1 file: {}", out_r1);
-    info!("output selected read2 file: {}", out_r2);
+
     let mut out_writer1 =
-        file_writer(Some(out_r1), compression_level, stdout_type).map(fastq::Writer::new)?;
+        file_writer(Some(out_r1), compression_level, stdout_type)?;
     let mut out_writer2 =
-        file_writer(Some(out_r2), compression_level, stdout_type).map(fastq::Writer::new)?;
+        file_writer(Some(out_r2), compression_level, stdout_type)?;
+        
     let (mut pe_r1, mut pe_r2) = (0usize, 0usize);
+    let intersect: HashSet<_> = read1_id.intersection(&read2_id).collect();
 
-    let fq_reader1 = file_reader(Some(fq1)).map(fastq::Reader::new)?;
-    for rec in fq_reader1.records().map_while(Result::ok) {
-        if read1_id.contains_key(rec.id()) && read2_id.contains_key(rec.id()) {
-            pe_r1 += 1;
-            out_writer1.write_record(&rec)?;
+    let mut fq_reader1 = file_reader(Some(fq1)).map(fastq::Reader::new)?;
+    info!("output selected read1 file: {}", out_r1);
+    while rset1.fill(&mut fq_reader1)? {
+        for rec in rset1.iter().map_while(Result::ok) {
+            if intersect.contains(&rec.id().to_owned()) {
+                pe_r1 += 1;
+                write_record(&mut out_writer1, rec.id(), rec.seq(), rec.qual())?;
+            }
         }
     }
     out_writer1.flush()?;
-
-    let fq_reader2 = file_reader(Some(fq2)).map(fastq::Reader::new)?;
-    for rec in fq_reader2.records().map_while(Result::ok) {
-        if read2_id.contains_key(rec.id()) && read1_id.contains_key(rec.id()) {
-            pe_r2 += 1;
-            out_writer2.write_record(&rec)?;
+    
+    let mut fq_reader2 = file_reader(Some(fq2)).map(fastq::Reader::new)?;
+    info!("output selected read2 file: {}", out_r2);
+    while rset2.fill(&mut fq_reader2)? {
+        for rec in rset2.iter().map_while(Result::ok) {
+            if intersect.contains(&rec.id().to_owned()) {
+                pe_r2 += 1;
+                write_record(&mut out_writer2, rec.id(), rec.seq(), rec.qual())?;
+            }
         }
     }
+
     out_writer2.flush()?;
     assert_eq!(pe_r1, pe_r2);
 
